0c7a145396348be66f84c37a5008b62423c82c98
Piotr Ganicz <piotr.ganicz@gmail.com>
2017-01-12 11:48:39 +0100

    Gauss neighbourhood generator, small style changes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from enum import Enum
 from faker import Factory
-from random import randint, random
+from random import randint, random, gauss
 import math
 
 NUMBER_OF_CITIES = 40
@@ -8,13 +7,13 @@ NUMBER_OF_YEARS = 10
 
 fakeFactory = Factory.create()
 
+
 class Day():
     def __init__(self, destination, flight_prices, sleeping_price, destination_weather_historic_data):
         self.destination = destination
         self.flight_price_to, self.flight_price_from = flight_prices
         self.destination_weather_historic_data = destination_weather_historic_data
         self.sleeping_price = sleeping_price
-
 
 
 def get_cities_prices(city_list):
@@ -25,15 +24,12 @@ def get_cities_prices(city_list):
     return city_price_list
 
 
-
-
 class HistoricDataGenerator:
     def __init__(self, year_number):
         w, h = 365, year_number
         self.city_historic_days = {}
         self.years = year_number
         self.assing_cities(year_number)
-
 
     def assing_cities(self, year_number):
         unique_names = set()
@@ -76,7 +72,7 @@ class CurrentFlightGenerator:
 
 
 class DataPreprocesor():
-    def __init__(self, city_historic_days, year_flights, city_list,city_price_list, years):
+    def __init__(self, city_historic_days, year_flights, city_list, city_price_list, years):
         self.city_historic_days = city_historic_days
         self.years = years
         self.city_list = city_list
@@ -85,7 +81,6 @@ class DataPreprocesor():
         # self.calculate_wheather_factor()
         # self.predict_city_wheather()
         self.generate_search_space()
-
 
     def generate_search_space(self):
         search_space = {}
@@ -105,14 +100,13 @@ class DataPreprocesor():
         return search_space
 
 
-
 class UrlopPlaner():
     def __init__(self, search_space, temperature, weather_importance, vacation_period):
         self.search_space = search_space
         self.temperature = temperature
         self.weather_importance = weather_importance
         self.vacation_period = vacation_period
-
+        self.city_list = list(self.search_space.keys())
 
     def calculate_sum(self, all_days):
         days_values = []
@@ -128,10 +122,10 @@ class UrlopPlaner():
                     pass
                 else:
                     raise Exception("That should not happen")
-            days_values.append((good_days/NUMBER_OF_YEARS, (neutral_days + good_days)/NUMBER_OF_YEARS))
+            days_values.append((good_days / NUMBER_OF_YEARS, (neutral_days + good_days) / NUMBER_OF_YEARS))
 
         all_days_predicted_wheather = [-1] * len(days_values)
-        for tuple_index,tuple in enumerate(days_values):
+        for tuple_index, tuple in enumerate(days_values):
             random_factor = random()
             for index, value in enumerate(tuple):
                 if random_factor > value:
@@ -151,51 +145,46 @@ class UrlopPlaner():
         return wheather_value
 
     def stopCondition(self, current_iteration):
-        if current_iteration == 1000000000:
+        if current_iteration == 1000:
             return True
         return False
 
-    def bad_pointy_acceptance_probability(self,current_temprature, qX, qY):
-        return math.exp(-abs((qY-qX)/current_temprature))
+    def bad_pointy_acceptance_probability(self, current_temprature, qX, qY):
+        return math.exp(-abs((qY - qX) / current_temprature))
 
     def q(self, coordinates):
         day_number, destination = coordinates
-        all_days = self.search_space[destination][day_number: day_number+self.vacation_period]
+        all_days = self.search_space[destination][day_number: day_number + self.vacation_period]
 
         period_weather_factor = self.calculate_sum(all_days)
 
-        objective_function = 1/((all_days[0].flight_price_to + all_days[-1].flight_price_from + (self.vacation_period * all_days[0].sleeping_price)) * (1 + ((self.weather_importance/self.vacation_period) * period_weather_factor)))
+        objective_function = 1 / ((all_days[0].flight_price_to + all_days[-1].flight_price_from + (
+        self.vacation_period * all_days[0].sleeping_price)) * (
+                                  1 + ((self.weather_importance / self.vacation_period) * period_weather_factor)))
         return objective_function
 
-
-    def select_neighbour(self, current_point):
+    def select_neighbour(self, current_point, sigma):
         day, city = current_point
-
-        random_factor = random()
         city_index = self.city_list.index(city)
 
-        if 0 <= random_factor < 0.25:
-            return (abs(day + 1) % 365, city)
-        elif 0.25 <= random_factor < 0.5:
-            return (abs(day - 1) % 365, city)
-        elif 0.5 <= random_factor < 0.75:
-            new_city = self.city_list[(city_index + 1) % NUMBER_OF_CITIES]
-            return (day, new_city)
-        elif 0.75 <= random_factor <= 1:
-            new_city = self.city_list[abs(city_index - 1) % NUMBER_OF_CITIES]
-            return (day, new_city)
+        new_day = math.floor(gauss(day, sigma))
+        new_city_idx = math.floor(gauss(city_index, sigma))
+        while ((0 <= new_day < 365) is False) or ((0 <= new_city_idx < len(self.city_list)) is False):
+            new_day = math.floor(gauss(day, sigma))
+            new_city_idx = math.floor(gauss(city_index, sigma))
+
+        return new_day, self.city_list[new_city_idx]
 
     def simulated_annealing(self):
         current_iteration = 1
         beginning_temp = self.temperature
         current_temperature = self.temperature
-        self.city_list = list(self.search_space.keys())
         random_day = randint(0, 365)
-        random_city = self.city_list[randint(0,len(self.city_list)-1)]
+        random_city = self.city_list[randint(0, len(self.city_list) - 1)]
 
         current_point = (random_day, random_city)
         while not self.stopCondition(current_iteration):
-            neighbour = self.select_neighbour(current_point)
+            neighbour = self.select_neighbour(current_point, 10.0)
             if not neighbour:
                 print("test")
             qX = self.q(current_point)
@@ -206,17 +195,15 @@ class UrlopPlaner():
                 current_point = neighbour
             print(neighbour)
             current_iteration += 1
-            current_temperature = beginning_temp/current_iteration
-
-
-
+            current_temperature = beginning_temp / current_iteration
 
 
 historicData = HistoricDataGenerator(NUMBER_OF_YEARS)
 flightGenerator = CurrentFlightGenerator(historicData.get_city_list())
 year_flights = flightGenerator.generate_flights()
 
-dataPreprocesor = DataPreprocesor(historicData.city_historic_days, year_flights, historicData.get_city_list(), get_cities_prices(historicData.get_city_list()),NUMBER_OF_YEARS)
+dataPreprocesor = DataPreprocesor(historicData.city_historic_days, year_flights, historicData.get_city_list(),
+                                  get_cities_prices(historicData.get_city_list()), NUMBER_OF_YEARS)
 
 urlopPlanes = UrlopPlaner(dataPreprocesor.generate_search_space(), 100, 0.3, 6)
 urlopPlanes.simulated_annealing()
